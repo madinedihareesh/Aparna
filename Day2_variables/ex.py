e35b9d89f00a440577fc16cf889ed4f02bdfdee3
Hareesh <hareesh.madinedi@gmail.com>
2026-02-19 18:59:42 +0530

    Day2_variables

---
+++ Day2_variables/ex.py
@@ -0,0 +1,78 @@
+# a=10
+# print(a)
+# A='Some Name'
+# print(A)
+# _a='somename2'
+# print(_a)
+# _A='somename3'
+# print(_A)
+# _1A='Somename4'
+# print(_1A)
+# _1a='somename5'
+# print(_1a)
+
+# a='AchieversIT'
+# print(a)
+
+
+'''
+oop:Object oriented programing (ruby)
+'''
+# Data: value,information
+# Data types: 
+'''
+1.primitive data(holding a sclar,single)
+2.non-primitve data
+'''
+'''
+Python:
+Python is High-level(scripting) object oriented interpriter based programing languge,it is dinamically typed
+ case-sencitive it is easy to learn
+'''
+
+# a=10 
+# #(identifier(a)=intiger object)
+# print(type(a))
+# print(dir(a))
+# print(id(a))
+# print(a.__sizeof__())
+# a=20
+# print(id(a))
+# print(a.__sizeof__())
+
+'''
+class: it is nothing but a blue print of an object
+class is a combination of properties and methods
+class name of class:()
+   var=value(idenitfier=data)
+   fuction (blcok of code used to complete a process/fuctionality)
+'''
+
+# primitive data type are immutable(can not be changed)
+a=10
+# intiger object 
+print(id(a))
+a=a+10
+# new object is created
+print(type(10))
+
+
+l=[1,2,3,4,5,6]
+print(type(l))
+print(id(l))
+l.pop()
+print(l)
+print(id(l))
+
+'''
+int 
+str
+bool
+complex
+float
+'''
+
+'''
+Sequencial [](sub-script oparator)(index)
+non sequentical( not having index)
+'''
