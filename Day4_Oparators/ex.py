db6fb22d3f36d229781c8fdc6a0d52305bd3497d
Hareesh <hareesh.madinedi@gmail.com>
2026-02-23 18:20:59 +0530

    Day4_Oparators

---
+++ Day4_Oparators/ex.py
@@ -0,0 +1,44 @@
+# Arthamatic
+print(10+20)
+print('Achievers'+'IT')
+print(20-10)
+print(10*2)
+print('*'*3)
+print(10/2)
+print(10%2)
+print(7//2)
+print(10**2)
+
+# Relational oparators:
+print(10<=20)
+
+# Assigment oparators
+a=10
+a+=1 
+a-=1
+a*=2
+a/=2
+print(a)
+a//=2
+print(a//2)
+a**=3
+print(a)
+
+# logical
+a=True
+b=False
+print(a or b)
+print(b or a)
+print(a or a)
+print(b or b)
+print((10>20) and (20<30))
+print(not(b))
+
+name='AchieversIT'
+print('H' in name)
+
+a=10
+print(id(a))
+b=20
+print(id(b))
+print(a is not b)
