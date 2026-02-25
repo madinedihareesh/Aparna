8717384592b78128dff8b10e7d0744b7b1c88dfd
Hareesh <hareesh.madinedi@gmail.com>
2026-02-25 18:28:33 +0530

    Day6_forloop

---
+++ Day6_Forllop/ex.py
@@ -0,0 +1,41 @@
+'''
+for item in sequence/range:
+    block of code
+'''
+
+# name='AchieversIT'
+# i=0
+# while i<len(name):
+#     print(name[i])
+#     i+=1
+
+'''
+for(int i=0;i<name.length();i++){
+}
+for each loop
+refer to each an every from a sequence
+'''
+# for item in name:
+#     print(item)   
+
+# l=['ram','ravi','rahul']
+# for i in l:
+#     print(i) 
+
+# range(start=0,stop,step=1)
+
+# for i in range(1,10,1):
+#     print(i)  
+
+# for i in range(1,101,1):
+#     print(i)
+
+num=int(input('Enter a number for printing a table:'))
+l=[1,2,3,4,5,6,7,8,9,10]
+for i in l:
+    print(num,'*',i,'=',(num*i))
+
+
+
+   
+
