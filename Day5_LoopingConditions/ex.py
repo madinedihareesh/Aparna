8825a298881d55c89682ff5cb75a07eaeffdf747
Hareesh <hareesh.madinedi@gmail.com>
2026-02-25 17:40:26 +0530

    Day5_whileloop

---
+++ Day5_LoopingConditions/ex.py
@@ -0,0 +1,20 @@
+i=1
+while i<6:
+    print('* '*i) 
+    i+=1
+
+i=5
+while i>0:
+    print('* '*i)
+    i-=1  
+
+i=0
+while i<6:
+    print(' '*(5-i)+'* '*i)
+    i+=1
+i=5
+while i>0:
+    print(' '*(5-i)+'* '*i)
+    i-=1
+
+
