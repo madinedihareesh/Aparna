47ec408046440c36eb0554115f49ff579424a67b
Hareesh <hareesh.madinedi@gmail.com>
2026-02-27 18:18:47 +0530

    Day8_programs

---
+++ Day8_Loops_and_conditions/ex.py
@@ -0,0 +1,88 @@
+# print the digits from a number
+# number=1234
+# while number>0:
+#     res=number%10
+#     print(res)
+#     number//=10
+
+# sum value of all the digits in number
+# number=1234
+# sum=0
+# while number>0:
+#     res=number%10
+#     sum+=res
+#     number//=10
+# print(sum)  
+
+# how check wether the number is a palendrome
+# number=int(input('Enter the number'))
+# input=number
+# out=0
+# while number>0:
+#     res=number%10
+#     out=out*10+res
+    
+#     number=number//10
+    
+# if out==input:
+#     print('It is a palndrome')
+# else:
+#     print('It is not a palndrome') 
+
+# plaendrome of a string
+# name=input('Enter a word')
+# count=len(name)-1
+# rev=''
+# while count>=0:
+#     rev+=name[count]
+#     count-=1
+# if name==rev: 
+#     print('It is a  palendrome')
+# else:
+#     print('it is not a palendrome')  
+
+# printing star insed of ovels
+# inputvalue=input('Enter the String')
+# ovles='AEIOUaeiou'
+# for i in inputvalue:
+#     if i in ovles:
+#         print('*',end='')
+#     else:
+#         print(i,end='')    
+
+# i=0
+# while i<len(inputvalue):
+#     if inputvalue[i] in ovles:
+#         print('*',end='')
+#     else:
+#         print(inputvalue[i],end='')
+#     i+=1        
+
+#print even numbers from 1 to 100
+# i=0
+# while i<=100:
+#     print(i,end=',')
+#     i+=2
+# print('')    
+
+# i=0
+# while i<=100:
+#     if i%2==0:
+#         print(i,end=',')
+#     i+=1
+# print('')    
+
+# for i in range(0,101,2):
+#     print(i,end=',')
+# print('')    
+
+# for i in range(0,101):
+#     if i%2==0:
+#         print(i,end=',')
+# print('')        
+
+    
+
+
+
+
